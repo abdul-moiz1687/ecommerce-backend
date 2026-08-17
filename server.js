54c93fb0ddcd1387f380b9798b868ccb82729e73
abdul-moiz1687 <abdulmoizarain766@gmail.com>
2026-08-17 21:59:43 +0500

    complete backend product api

--- server.js
+++ server.js
@@ -9,7 +9,4 @@ app.use(express.json())
 
 app.use('/api/products',pruductRoutes)
 
-const PORT = 4000;
-app.listen(PORT,()=>{
-    console.log(`Server is running on port:${PORT}`)
-})
+module.exports = app;
